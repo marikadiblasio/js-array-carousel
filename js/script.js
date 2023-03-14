d447a3956542a93a988d8e9a93e581323c409c03
marikadiblasio <marbla.albram@gmail.com>
2023-03-14 14:59:36 +0100

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/*
+Consegna:
+Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+MILESTONE 1
+Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un'immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.*/
